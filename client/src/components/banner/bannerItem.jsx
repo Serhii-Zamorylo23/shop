8c54dff9d9a46b5fba90dc6e135ba030ad6e8b9d
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2024-07-03 23:49:37 +0300

    г

--- client/src/components/banner/bannerItem.jsx
+++ client/src/components/banner/bannerItem.jsx
@@ -1,6 +1,18 @@
 import s from "./banner.module.scss"
+import image from "./image 3.png"
+const img = image
 const BannerItem=()=>{
-  <div className={s.banner_firstCont}>fd</div>
-
+  return(
+    <>
+    <div className={s.banner_firstCont}>
+    <h1 className={s.banner_firstCont_title}>Freshly pressed,straight to your door</h1>
+    <p className={s.banner_firstCont_text}>At Pure Flave, we’re dedicated to goodness in every sip. Delicious, fresh juice, just as nature intended.</p>
+    <button className={s.banner_firstCont_button}>Shop</button>
+    </div>
+    <div className={s.banner_secondCont}>
+      <img src={img} alt="" />
+    </div>
+    </>
+  )
 }
 export default BannerItem
